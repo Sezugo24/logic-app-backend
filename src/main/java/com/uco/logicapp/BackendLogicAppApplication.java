a48bff63ce0523720534370c66a56340ec84871d
Sebastián Zuluaga Gómez - Ceiba Software <sebastian.zuluaga@ceiba.com.co>
2021-09-19 17:02:01 -0500

    Estructura inicial

---
+++ src/main/java/com/uco/logicapp/BackendLogicAppApplication.java
@@ -0,0 +1,13 @@
+package com.uco.logicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLogicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLogicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uco/logicapp/BackendLogicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.logicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLogicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
