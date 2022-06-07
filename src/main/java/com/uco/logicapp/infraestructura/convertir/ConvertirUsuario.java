4fcd6d7cff1cbca083be0e0578b8b7fae36a053b
sezugo <sezugo@gmail.com>
2022-06-06 19:55:18 -0500

    Se agrega clase convertirIUsuario

--- src/main/java/com/uco/logicapp/infraestructura/convertir/ConvertirUsuario.java
+++ src/main/java/com/uco/logicapp/infraestructura/convertir/ConvertirUsuario.java
@@ -1,8 +1,32 @@
 package com.uco.logicapp.infraestructura.convertir;
 
+import com.uco.logicapp.dominio.modelo.dto.UsuarioDTO;
+import com.uco.logicapp.dominio.modelo.entidad.Usuario;
+import com.uco.logicapp.infraestructura.entidades.EntidadUsuario;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ConvertirUsuario {
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    public EntidadUsuario convertirDominioPorEntidad(Usuario usuario){
+        return modelMapper.map(usuario, EntidadUsuario.class);
+    }
+
+    public UsuarioDTO convertirEntidadUsuarioPorDominio(EntidadUsuario entidadUsuario){
+        return modelMapper.map(entidadUsuario, UsuarioDTO.class);
+    }
+
+    public List<UsuarioDTO> convertirListaEntidadUsuarioPorListaCliente(List<EntidadUsuario> listaEntidadUsuario, List<UsuarioDTO> listaUsuario){
+        for(EntidadUsuario entidadUsuario: listaEntidadUsuario){
+            UsuarioDTO usuario = modelMapper.map(entidadUsuario, UsuarioDTO.class);
+            listaUsuario.add(usuario);
+        }
+        return listaUsuario;
+    }
 
 }
