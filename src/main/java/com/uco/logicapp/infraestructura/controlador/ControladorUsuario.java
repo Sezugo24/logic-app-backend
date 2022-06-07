e0a3c74bae6feec3a0ec1062884cb9e565bcd35c
Cristián Ramírez <cristian.566.ra@hotmail.com>
2022-06-06 19:17:07 -0500

    Se creo la clase controladorUsuario

--- src/main/java/com/uco/logicapp/infraestructura/controlador/ControladorUsuario.java
+++ src/main/java/com/uco/logicapp/infraestructura/controlador/ControladorUsuario.java
@@ -1,10 +1,48 @@
 package com.uco.logicapp.infraestructura.controlador;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.uco.logicapp.aplicacion.comando.ComandoUsuario;
+import com.uco.logicapp.aplicacion.comando.manejador.usuario.ManejadorActualizarUsuario;
+import com.uco.logicapp.aplicacion.comando.manejador.usuario.ManejadorCrearUsuario;
+import com.uco.logicapp.aplicacion.comando.manejador.usuario.ManejadorEliminarUsuario;
+import com.uco.logicapp.aplicacion.consulta.ManejadorListarUsuario;
+import com.uco.logicapp.dominio.modelo.dto.UsuarioDTO;
+import com.uco.logicapp.dominio.modelo.entidad.Usuario;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(name = "/usuario")
 public class ControladorUsuario {
-    
+    private final ManejadorCrearUsuario manejadorCrearUsuario;
+    private final ManejadorActualizarUsuario manejadorActualizarUsuario;
+    private final ManejadorEliminarUsuario manejadorEliminarUsuario;
+    private final ManejadorListarUsuario manejadorListarUsuario;
+
+    public ControladorUsuario(ManejadorCrearUsuario manejadorCrearUsuario, ManejadorActualizarUsuario manejadorActualizarUsuario, ManejadorEliminarUsuario manejadorEliminarUsuario, ManejadorListarUsuario manejadorListarUsuario) {
+        this.manejadorCrearUsuario = manejadorCrearUsuario;
+        this.manejadorActualizarUsuario = manejadorActualizarUsuario;
+        this.manejadorEliminarUsuario = manejadorEliminarUsuario;
+        this.manejadorListarUsuario = manejadorListarUsuario;
+    }
+
+    @PostMapping
+    public void crearUsuario(@RequestBody ComandoUsuario comandoUsuario){
+        this.manejadorCrearUsuario.ejecutar(comandoUsuario);
+    }
+
+    @GetMapping
+    public List<UsuarioDTO> listarUsuario(){
+        return this.manejadorListarUsuario.ejecutar();
+    }
+
+    @PutMapping
+    public void actualizarUsuario(@RequestBody ComandoUsuario comandoUsuario){
+        this.manejadorActualizarUsuario.ejecutar(comandoUsuario);
+    }
+
+    @DeleteMapping
+    public void eliminarUsuario(@PathVariable Integer id){
+        this.manejadorEliminarUsuario.ejecutar(id);
+    }
 }
