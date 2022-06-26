62289e3eef19d109e8e3ebda203023affc74638e
sezugo <sezugo@gmail.com>
2022-06-26 14:08:28 -0500

    Se quitan import que no se utilizan

--- src/main/java/com/uco/logicapp/BackendLogicAppApplication.java
+++ src/main/java/com/uco/logicapp/BackendLogicAppApplication.java
@@ -1,7 +1,5 @@
 package com.uco.logicapp;
 
-import com.uco.logicapp.dominio.modelo.dto.DtoUsuario;
-import com.uco.logicapp.dominio.modelo.entidad.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
